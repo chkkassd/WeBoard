8ed610b2fd497674cd1b8586d7808cca909d5746
Peter Shi <532289386@qq.com>
2018-06-06 17:34:35 +0800

    集成了自己开发SSFGraphKit动态库

---
+++ WeBoard/Main/Controller/TestViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TestViewController.swift
+//  WeBoard
+//
+//  Created by 赛峰 施 on 2018/6/6.
+//  Copyright © 2018年 赛峰 施. All rights reserved.
+//
+
+import UIKit
+import SSFGraphKit
+
+class TestViewController: UIViewController {
+
+    var graphView : SSFScatterGraphView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let data = [("Shanghai", 2500.0),("Beijing", 3200.0),("Houston", 300.0),("New York", 1500.0),("Berlin", 1400.0)]
+        graphView = SSFScatterGraphView(frame: CGRect(x: 0, y: 50, width: UIScreen.main.bounds.width, height: 400), sourceData: data)
+        graphView.barColor = UIColor.green
+        graphView.textColor = UIColor.red
+        graphView.textFont = UIFont.systemFont(ofSize: 16)
+        graphView.scatterColor = UIColor.orange
+        //        graphView.scatterType = .square
+        //        graphView.isOutstanding = true
+        self.view.addSubview(graphView)
+    }
+
+
+}
